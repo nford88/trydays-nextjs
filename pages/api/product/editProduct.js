2bd9ac9be1ac3667c817cb691744f4ea5ea1665f
Nick Ford <nickford@MacBook-Pro.local>
2020-04-02 16:55:18 +0100

    Edit Record Fix

--- pages/api/product/editProduct.js
+++ pages/api/product/editProduct.js
@@ -1,12 +1,19 @@
 import { client, q } from '../../config/db'
 
-const editProduct = (prodRef, newName) => client.query(
-  q.Update(
-    q.Ref(q.Collection('products'), prodRef),
-    { data: { newName: newName } },
-  )
-)
-.then((ret) => console.log(ret))
-.catch(err => console.warn(err))
+
+const editProduct = async (req, res) => {
+  const { prodRef, values } = req.body;
+  try {
+    const dbs = await client.query(
+      q.Update(
+        q.Ref(q.Collection('products'), prodRef),
+        { data: values },
+      )
+    )
+    res.status(200).json({ message: 'Updated Succesfully', prodRef: prodRef, values: values })
+  } catch (e) {
+    res.status(500).json({ error: e.message, prodRef: prodRef })
+  }
+}
 
 export default editProduct
