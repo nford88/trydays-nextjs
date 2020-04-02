316eb29f6a6970781818dda8ccdffabea09787fc
Nick Ford <nickford@MacBook-Pro.local>
2020-04-02 16:14:16 +0100

    Delete Fixed

--- pages/api/product/deleteProduct.js
+++ pages/api/product/deleteProduct.js
@@ -1,9 +1,20 @@
-import { client, q } from '../config/db'
+import { client, q } from '../../config/db'
 
-const deleteProduct = prodRef => client.query(
-  q.Delete(q.Ref(q.Collection('products'), prodRef))
-)
-.then(res => res)
-.catch(err => console.warn(err.message))
+
+const deleteProduct = async (req, res) => {
+  const { prodRef } = req.body;
+  try {
+    const dbs = await client.query(
+      q.Delete(
+        q.Ref(
+          q.Collection('products'), prodRef
+        )
+      )
+    )
+    res.status(200).json({ prodRef: prodRef, message: 'Deleted Succesfully' })
+  } catch (e) {
+    res.status(500).json({ error: e.message, prodRef: prodRef })
+  }
+}
 
 export default deleteProduct
